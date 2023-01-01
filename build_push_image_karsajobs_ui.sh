e3c55541e5e8bfc01bb3aebe545d13e5424400b7
Aziz Rosyid <azizrosyid783@gmail.com>
2023-01-02 01:38:40 +0700

    fix

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,8 @@
+# membuat image docker dengan tag github package registry
+docker build -t ghcr.io/azizrosyid/karsajobs-ui:latest .
+
+# login ke github package registry
+echo $PASSWORD_GITHUB_PKG | docker login ghcr.io -u azizrosyid --password-stdin
+
+# push image ke github package registry
+docker push ghcr.io/azizrosyid/karsajobs-ui:latest
